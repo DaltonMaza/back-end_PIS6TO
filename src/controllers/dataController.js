068c0b93e222716b25e690ff06702c005c61c43e
DJRP-U <diego.j.riofrio@unl.edu.ec>
2024-07-20 08:38:26 -0500

    corregido error dataController

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,60 +1,64 @@
 const Data = require("../models/Data");
+
 module.exports = {
-    createData : async (req, res) => {
-        try {
-          console.log(req)
-          const Data = new Data(req.body);
-          await Data.save();
-          res.status(201).send(Data);
-        } catch (error) {
-          res.status(400).send(error);
-        }
-      },
-      
-      getAllData : async (req, res) => {
-        try {
-          const Data = await Data.find();
-          console.log(Data)
-          res.status(200).send(Data);
-        } catch (error) {
-          res.status(500).send(error);
-        }
-      },
-      
-      getDataById : async (req, res) => {
-        try {
-          const Data = await Data.findById(req.params.id);
-          if (!Data) {
-            return res.status(404).send();
-          }
-          res.status(200).send(Data);
-        } catch (error) {
-          res.status(500).send(error);
-        }
-      },
-      
-      updateData : async (req, res) => {
-        try {
-          const Data = await Data.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-          if (!Data) {
-            return res.status(404).send();
-          }
-          res.status(200).send(Data);
-        } catch (error) {
-          res.status(400).send(error);
-        }
-      },
-      
-      deleteData : async (req, res) => {
-        try {
-          const Data = await Data.findByIdAndDelete(req.params.id);
-          if (!Data) {
-            return res.status(404).send();
-          }
-          res.status(200).send(Data);
-        } catch (error) {
-          res.status(500).send(error);
-        }
+  createData: async (req, res) => {
+    try {
+      const dataInstance = new Data({
+        humidity: req.body.humidity,
+        temperature: req.body.temperature
+      });
+      console.log(dataInstance);
+      await dataInstance.save();
+      res.status(201).send(dataInstance);
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({ error: error.message }); // Enviar error como objeto JSON
+    }
+  },
+
+  getAllData: async (req, res) => {
+    try {
+      const allData = await Data.find();
+      console.log(allData);
+      res.status(200).send(allData);
+    } catch (error) {
+      res.status(500).send({ error: error.message }); // Enviar error como objeto JSON
+    }
+  },
+
+  getDataById: async (req, res) => {
+    try {
+      const dataInstance = await Data.findById(req.params.id);
+      if (!dataInstance) {
+        return res.status(404).send();
       }
-      
-}
+      res.status(200).send(dataInstance);
+    } catch (error) {
+      res.status(500).send({ error: error.message }); // Enviar error como objeto JSON
+    }
+  },
+
+  updateData: async (req, res) => {
+    try {
+      const dataInstance = await Data.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+      if (!dataInstance) {
+        return res.status(404).send();
+      }
+      res.status(200).send(dataInstance);
+    } catch (error) {
+      res.status(400).send({ error: error.message }); // Enviar error como objeto JSON
+    }
+  },
+
+  deleteData: async (req, res) => {
+    try {
+      const dataInstance = await Data.findByIdAndDelete(req.params.id);
+      if (!dataInstance) {
+        return res.status(404).send();
+      }
+      res.status(200).send(dataInstance);
+    } catch (error) {
+      res.status(500).send({ error: error.message }); // Enviar error como objeto JSON
+    }
+  }
+};
